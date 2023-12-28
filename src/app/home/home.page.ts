035d8903fa8e8a9bd79be600d87b13cd8dc68932
Henrique Mendes <121140131+HenriqueMendes77@users.noreply.github.com>
2023-12-28 10:04:52 -0300

    iniciado desenvolvimento da interface e funcionalidade

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +7,112 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor() { }
 
+  tela: string = '';
+  b: string = '';
+  temp: any;
+  numeros: any = [];
+  resultado = 0;
+
+  soma(numeros: any) {
+    numeros.forEach((n: any) => {
+      this.resultado += n;
+    });
+  }
+
+  sub(numeros: any) {
+    this.resultado = numeros[0];
+    numeros.shift();
+    numeros.forEach((n: any) => {
+      this.resultado -= n;
+    });
+  }
+
+  mult(numeros: any) {
+    this.resultado = numeros[0];
+    numeros.shift();
+    numeros.forEach((n: any) => {
+      this.resultado *= n;
+    });
+  }
+
+  divi(numeros: any) {
+    this.resultado = numeros[0];
+    numeros.shift();
+    numeros.forEach((n: any) => {
+      this.resultado /= n;
+    });
+  }
+
+  ngOnInit(): void {
+
+  }
+
+  selOperacao(op: any) {
+    
+    if (op == 'limpa') {
+      this.tela = '';
+      this.numeros = [];
+      this.temp = null;
+      this.b = '';
+    }
+    else {
+      this.b = op;
+      this.tela += op;
+    }
+
+    console.log(op);
+  }
+
+  selNumero(num: any) {
+    this.tela += num;
+    this.numeros.push(num);
+
+    if(this.b == '+'){
+      this.soma(this.numeros);
+      this.numeros.shift();
+      this.numeros.shift();
+      this.temp = this.resultado;
+      this.numeros.unshift(this.temp);
+      this.resultado = 0;
+      this.b = '';
+    }
+
+    if(this.b == '-'){
+      this.sub(this.numeros);
+      this.numeros.shift();
+      this.numeros.shift();
+      this.temp = this.resultado;
+      this.numeros.unshift(this.temp);
+      this.resultado = 0;
+      this.b = '';
+    }
+
+    if(this.b == '*'){
+      this.mult(this.numeros);
+      this.numeros.shift();
+      this.numeros.shift();
+      this.temp = this.resultado;
+      this.numeros.unshift(this.temp);
+      this.resultado = 0;
+      this.b = '';
+    }
+
+    if(this.b == '/'){
+      this.divi(this.numeros);
+      this.numeros.shift();
+      this.numeros.shift();
+      this.temp = this.resultado;
+      this.numeros.unshift(this.temp);
+      this.resultado = 0;
+      this.b = '';
+    }
+
+    console.log("num: "+num)
+    console.log("numeros: "+this.numeros)
+    console.log("temp: "+ this.temp)
+    console.log("resultado: "+ this.resultado)
+    console.log("b: "+ this.b)
+  }
 }
